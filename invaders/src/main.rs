8e9c66a33d9ff9b2515ddd7612642db28063fe58
Yang Yu <yy19@ualberta.ca>
2024-01-06 12:02:05 -0700

    ongoing update

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,6 +1,19 @@
 // Importing necessary modules and traits
-use std::error::Error;
+#![allow(dead_code, unused_imports, unused_variables)]
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{event, terminal, ExecutableCommand};
+// use invaders::frame::{new_frame, Drawable};
+// use invaders::invaders::Invaders;
+// use invaders::player::Player;
+// use invaders::{frame, render};
 use rusty_audio::Audio;
+use std::error::Error;
+use std::sync::mpsc;
+use std::time::{Duration, Instant};
+use std::{io, thread};
+
 
 // The main function with a return type that can accommodate any error that implements the `Error` trait.
 fn main() -> Result<(), Box<dyn Error>> {
@@ -9,23 +22,40 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Adding various sound effects to the audio handler. Each sound file is associated with a key.
     // The audio files are expected to be in the 'sounds' directory relative to the current working directory.
-    audio.add("explode", "sounds/explode.wav"); // Adding an 'explode' sound effect.
-    audio.add("lose", "sounds/lose.wav");       // Adding a 'lose' sound effect.
-    audio.add("move", "sounds/move.wav");       // Adding a 'move' sound effect.
-    audio.add("pew", "sounds/pew.wav");         // Adding a 'pew' sound effect.
-    audio.add("startup", "sounds/startup.wav"); // Adding a 'startup' sound effect.
-    audio.add("win", "sounds/win.wav");         // Adding a 'win' sound effect.
-
-    // Playing the 'startup' sound effect.
-    // This will start playing the sound file associated with the key "startup".
+    audio.add("explode", "assets/explode.wav"); // Adding an 'explode' sound effect.
+    audio.add("lose", "assets/lose.wav");       // Adding a 'lose' sound effect.
+    audio.add("move", "assets/move.wav");       // Adding a 'move' sound effect.
+    audio.add("pew", "assets/pew.wav");         // Adding a 'pew' sound effect.
+    audio.add("startup", "assets/startup.wav"); // Adding a 'startup' sound effect.
+    audio.add("win", "assets/win.wav");         // Adding a 'win' sound effect.
     audio.play("startup");
 
-    // Block the current thread until all sounds that are currently playing have finished.
-    // This is important to ensure that the program doesn't exit prematurely,
-    // cutting off sound playback.
-    audio.wait();
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    io::stdout().execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
 
-    // If everything executed successfully, return Ok.
-    // The '()' signifies that the function is not returning any meaningful value.
+    // GameLoop
+    'gameloop: loop {
+        // Input events
+        while event::poll(Duration::default())?{
+            if let Event::Key(key_event) = event:: read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') =>{
+                        audio.play("loose");
+                        break 'gameloop;
+                    }
+                    _ =>{}
+                }
+            }
+        }
+    }
+
+    // Clean up
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
